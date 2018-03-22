51b836172001916f978b15e65a7304893bf726c0
Satya RC <satyarchandra@gmail.com>
2018-03-22 16:11:45 +0530

    Port number to 3011

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3011);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jshtml');
 app.use(express.favicon());
